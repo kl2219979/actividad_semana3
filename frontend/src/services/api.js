9e49b85cdd319efd15924275d74e1c23d3e04127
KL2219979 <KL2219979@gmail.com>
2026-05-28 14:57:39 -0500

    modificacion a api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,17 @@
 // responsabilidad de comunicarse con la APi
 
 //url para personajes, ubicaciones y episodios
-const URL_CHARACTER = 'https://rickandmortyapi.com/api/character'
-const URL_LOCATION = ''
-const URL_EPISODES = ''
+const URL_CHARACTER = 'https://rickandmortyapi.com/api/character';
+const URL_LOCATION = 'https://rickandmortyapi.com/api/location';
+const URL_EPISODES = 'https://rickandmortyapi.com/api/episode';
 
 // funcion que trae los datos de la APi Rick and Morty
-async function fetchApi(url) {
+export async function fetchApi(url) {
     const response = await fetch(url)
     if (!response.ok) {
         const text = response.text().catch(() => response.statusText);
         throw new Error(`HTTP ${response.status}: ${text}`);
     }
-    const data = await response.json();
+    return response.json();
 }
 
